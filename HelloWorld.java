4a314e4e13d9533c28548495e3bef31d7ad28f0c
EC2 Default User <ec2-user@ip-172-31-2-157.ap-south-1.compute.internal>
2021-05-13 06:05:16 +0000

    Lines added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to jenkis practice welllll....."); 
+        System.out.println("Hello, Welcome to linux practice welllll....."); 
+        System.out.println("Hello, Welcome to shellscripting practice welllll....."); 
+        System.out.println("Hello, Welcome to aws practice welllll....."); 
+        System.out.println("Hello, Welcome to ansible  practice welllll....."); 
+        System.out.println("Hello, Welcome to terraform  practice welllll....."); 
+
     }
 }
